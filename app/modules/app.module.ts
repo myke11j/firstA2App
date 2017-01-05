24ed080d7f1f5109453a753f2c12d67358acb1d7
myke11j <mukul1904@gmail.com>
2017-01-05 11:16:48 +0530

    Removing typescript transpiled JS and MAP file

--- app/modules/app.module.ts
+++ app/modules/app.module.ts
@@ -14,6 +14,7 @@ import { AppRoutingModule }     from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
+
     AppRoutingModule
   ],
   // This array contains the list of all components, pipes, and directives
