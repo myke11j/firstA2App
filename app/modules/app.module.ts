e762150fc1ec2a569e9ac5e4b911cf93f1c89a32
myke11j <mukul1904@gmail.com>
2017-01-11 12:49:24 +0530

    Mock API library added

--- app/modules/app.module.ts
+++ app/modules/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent }  from '../components/app.component';
 import { HeroDetailComponent }  from '../components/hero-detail/hero-detail.component';
@@ -10,12 +11,18 @@ import { HeroService }  from '../services/hero.service';
 
 import { AppRoutingModule }     from './app-routing.module';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from '../mock-server/in-memory-data.service';
+
 @NgModule({
   // Import all modules in this array
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    HttpModule
   ],
   // This array contains the list of all components, pipes, and directives
   // that we created and that belong in our application's module
